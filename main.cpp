65cea2f542daa704bb8a48e8732ea94d1eeefaa7
Enn Sunset <ennsunset@Enn-Mr-Sunsets-MacBook-Pro.local>
2024-12-06 22:52:18 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1 = 5;
+    int num2 = 10;
+
+    int sum = num1 + num2;
+
+    cout<<"I am a boy"<<endl;
+
+    cout<< "The sum of the two numbers is " << sum <<endl;
+    return 0;
+}a64l
